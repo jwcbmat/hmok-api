40665810dcd792555125866b9f2280ed4e8a5431
jwcbmat <johnathan.wcb@gmail.com>
2022-02-21 19:50:39 -0300

    adding cors in my header

--- index.js
+++ index.js
@@ -7,11 +7,20 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+
 app.get('/', (req, res) => {
     res.send('funcionando');
 });
 
-app.use('/api', rotes)
+app.use('/api', rotes);
 
 const port = 3001;
 
