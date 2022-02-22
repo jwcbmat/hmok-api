56a41f95a52376a8f892ca7433ee8b6a081c35f0
jwcbmat <johnathan.wcb@gmail.com>
2022-02-21 23:18:02 -0300

    index.js

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const rotes = require('./rotes');
+const clientData = require('./database');
+
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.get('/', (req, res) => {
-    res.send('funcionando');
+  res.send('funcionando');
 });
 
 app.use('/api', rotes);
@@ -25,5 +27,5 @@ app.use('/api', rotes);
 const port = 3001;
 
 app.listen(port, () => {
-    console.log('server running on http://localhost:', port);
+  console.log('server running on http://localhost:', port);
 })
